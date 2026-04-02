4c45ceeeca8cc346304a3ac50973405228bb86dd
madadani <wahyuramadanii27@gmail.com>
2026-04-02 14:08:55 +0700

    ci: fix vite config and disable strict CI

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,22 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: []
+      }
+    })
+  ],
   server: {
     host: true,
   },
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'UNUSED_EXTERNAL_IMPORT') return
+        warn(warning)
+      }
+    }
+  }
 })
